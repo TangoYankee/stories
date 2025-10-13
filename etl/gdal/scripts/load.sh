0020a553126ef1016773cfc8f3e9ba52a4d1b795
Timothy Miller <miller.tim108@gmail.com>
2025-10-13 19:28:29 -0400

    input geojson for ccds

--- etl/gdal/scripts/load.sh
+++ etl/gdal/scripts/load.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 
 ogr2ogr -nln subway_ada PG:"dbname=$POSTGRES_DB host=etl-postgis-1 user=$POSTGRES_USER port=5432" data/2024_aug_22_nyc_subway_stations.geojson
+ogr2ogr -nln source_city_council_districts PG:"dbname=$POSTGRES_DB host=etl-postgis-1 user=$POSTGRES_USER port=5432" data/city-council-districts.geojson
