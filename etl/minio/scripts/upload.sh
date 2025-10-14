23065f7981cbd82ac9ee4e9636935bafd779492d
Timothy Miller <miller.tim108@gmail.com>
2025-10-13 20:03:15 -0400

    city council district pmtiles

--- etl/minio/scripts/upload.sh
+++ etl/minio/scripts/upload.sh
@@ -3,3 +3,4 @@
 source ./scripts/spaces_alias.sh
 
 mc put data/2024_aug_22_subway_ada.pmtiles spaces/tangoyankee/nyc_subway_stations/2024_aug_22_subway_ada.pmtiles
+mc put data/city-council-district.pmtiles spaces/tangoyankee/city-council-district.pmtiles
--- etl/tippecanoe/scripts/convert.sh
+++ etl/tippecanoe/scripts/convert.sh
@@ -1,3 +1,29 @@
 #! /bin/sh
 
-tippecanoe -zg --projection=EPSG:4326 -o data/2024_aug_22_subway_ada.pmtiles -l subway_station data/subway_ada.json 
+tippecanoe \
+    -z7 \
+    --projection=EPSG:4326 \
+    -o \
+    data/2024_aug_22_subway_ada.pmtiles \
+    -l subway_station data/subway_ada.json \
+    --force
+
+tippecanoe \
+    -z13 \
+    --projection=EPSG:4326 \
+    -o data/city-council-district-fill.pmtiles \
+    -l fill data/city_council_district_fill.geojson \
+    --force
+
+tippecanoe \
+    -z13 \
+    --projection=EPSG:4326 \
+    -o data/city-council-district-label.pmtiles \
+    -l label data/city_council_district_label.geojson \
+    --force
+
+tile-join \
+    -o data/city-council-district.pmtiles \
+    data/city-council-district-fill.pmtiles \
+    data/city-council-district-label.pmtiles \
+    --force
