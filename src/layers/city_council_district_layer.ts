8e73c5aa7bfa14b0cb4d075653215e6d3fca4925
Timothy Miller <miller.tim108@gmail.com>
2025-10-18 08:34:10 -0400

    tile the city council districts

--- src/layers/city_council_district_layer.ts
+++ src/layers/city_council_district_layer.ts
@@ -8,6 +8,16 @@ export type CityCouncilDistrict = {
   district: string;
   layer: "fill" | "label";
 };
+
+const fillColors = [
+  "rgba(141 ,211, 199, 0.05)",
+  "rgba(255, 255, 179, 0.05)",
+  "rgba(190, 186, 218, 0.05)",
+  "rgba(251, 128, 114, 0.05)",
+  "rgba(128, 177, 211, 0.05)",
+  "rgba(253, 180, 98, 0.05)",
+  "rgba(179, 222, 105, 0.05)",
+];
 export const cityCouncilDistrict = () =>
   new VectorTile({
     source: new PMTilesVectorSource({
@@ -16,17 +26,24 @@ export const cityCouncilDistrict = () =>
         `<a href="https://opendata.cityofnewyork.us/">NYC open data</a>`,
     }),
     style: (feature) => {
-      const { district, layer } = feature.getProperties() as CityCouncilDistrict;
+      const { id, district, layer } = feature
+        .getProperties() as CityCouncilDistrict;
       const style = layer === "label"
         ? {
           text: new Text({
             font: "12px Calibri,sans-serif",
             text: district,
+            fill: new Fill({
+              color:"rgba(25, 31, 52, 0.6)"
+            }),
           }),
         }
         : {
+          fill: new Fill({
+            color: fillColors[id % 7],
+          }),
           stroke: new Stroke({
-            color: "gray",
+            color: "rgba(25, 31, 52, 0.1)",
             width: 2,
           }),
         };
