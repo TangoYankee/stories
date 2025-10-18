2d5fca7675bef38724c040161f29c6a2f0fb57aa
Timothy Miller <miller.tim108@gmail.com>
2025-10-18 18:06:14 -0400

    first attempt at slider

--- src/panel/panel.tsx
+++ src/panel/panel.tsx
@@ -7,7 +7,73 @@ export function Panel(props: JSX.HTMLAttributes<HTMLDivElement>) {
   return (
     <div id="panel" {...props}>
       <div>
-        <h2>Subway Stations</h2>
+        <div
+          class={css({
+            display: "flex",
+            width: "100%",
+            justifyContent: "space-between",
+          })}
+        >
+          <h2>Subway Stations</h2>
+          <div
+            class={css({
+              height: "1rem",
+              width: "3rem",
+            })}
+          >
+            <input
+              type="checkbox"
+              checked
+              classList={{"peer": true}}
+              class={css({
+                position: "absolute",
+                height: "inherit",
+                width: "inherit",
+                opacity: 0,
+                cursor: "pointer",
+                zIndex: 2
+              })}
+            />
+            <label
+              for="stations-toggle"
+              aria-label="Subway Stations Switch"
+              class={css({
+                _peerChecked: {
+                  backgroundColor: "amber.500"
+                },
+                height: "inherit",
+                width: "inherit",
+                position: "absolute",
+                backgroundColor: "stone.200",
+                borderRadius: "sm",
+                borderStyle: "solid",
+                borderWidth: "thin",
+                borderColor: "zinc.800",
+                transition: "0.3s",
+                zIndex: 0
+              })}
+            />
+            <div class={css({
+              height: "1rem",
+              width: "1.2rem",
+              _peerChecked: {
+                marginLeft: "1.75rem",
+                borderLeftRadius: "unset",
+                borderRightRadius: "sm",
+                backgroundColor: "green.900"
+              },
+              borderLeftRadius: "sm",
+              transition: "0.3s",
+              borderStyle: "solid",
+              borderWidth: "thin",
+              borderColor: "zinc.800",
+              content: "off",
+              position: "absolute",
+              backgroundColor: "zinc.400",
+              zIndex: 1,
+            })}/>
+          </div>
+        </div>
         <Legend
           class={css({
             display: "flex",
