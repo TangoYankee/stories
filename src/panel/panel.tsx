3caeed23c61f7289b7556b7cbc655bb4f1989519
Timothy Miller <miller.tim108@gmail.com>
2025-10-18 14:14:57 -0400

    layers in panel

--- src/panel/panel.tsx
+++ src/panel/panel.tsx
@@ -3,7 +3,8 @@ import { JSX } from "solid-js/jsx-runtime";
 export function Panel(props: JSX.HTMLAttributes<HTMLDivElement>) {
   return (
     <div id="panel" {...props}>
-      Panel
+      <h2>Subway Stations</h2>
+      <h2>City Council Districts</h2>
     </div>
   )
 }
