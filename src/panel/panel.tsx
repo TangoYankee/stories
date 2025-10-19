620d0b822956b99681c79fd1cc191508882e535a
Timothy Miller <miller.tim108@gmail.com>
2025-10-18 22:20:37 -0400

    switch as superficial component

--- src/panel/panel.tsx
+++ src/panel/panel.tsx
@@ -2,6 +2,7 @@ import { JSX } from "solid-js/jsx-runtime";
 import { Legend } from "../legend/index.ts";
 // @ts-ignore .ts file not created by styled-system
 import { css } from "../../styled-system/css/index.d.ts";
+import { Switch } from "../switch/switch.tsx";
 
 export function Panel(props: JSX.HTMLAttributes<HTMLDivElement>) {
   return (
@@ -12,67 +13,11 @@ export function Panel(props: JSX.HTMLAttributes<HTMLDivElement>) {
             display: "flex",
             width: "100%",
             justifyContent: "space-between",
+            alignItems: "center",
           })}
         >
           <h2>Subway Stations</h2>
-          <div
-            class={css({
-              height: "1rem",
-              width: "3rem",
-            })}
-          >
-            <input
-              type="checkbox"
-              checked
-              classList={{"peer": true}}
-              class={css({
-                position: "absolute",
-                height: "inherit",
-                width: "inherit",
-                opacity: 0,
-                cursor: "pointer",
-                zIndex: 2
-              })}
-            />
-            <label
-              for="stations-toggle"
-              aria-label="Subway Stations Switch"
-              class={css({
-                _peerChecked: {
-                  backgroundColor: "amber.500"
-                },
-                height: "inherit",
-                width: "inherit",
-                position: "absolute",
-                backgroundColor: "stone.200",
-                borderRadius: "sm",
-                borderStyle: "solid",
-                borderWidth: "thin",
-                borderColor: "zinc.800",
-                transition: "0.3s",
-                zIndex: 0
-              })}
-            />
-            <div class={css({
-              height: "1rem",
-              width: "1.2rem",
-              _peerChecked: {
-                marginLeft: "1.75rem",
-                borderLeftRadius: "unset",
-                borderRightRadius: "sm",
-                backgroundColor: "green.900"
-              },
-              borderLeftRadius: "sm",
-              transition: "0.3s",
-              borderStyle: "solid",
-              borderWidth: "thin",
-              borderColor: "zinc.800",
-              content: "off",
-              position: "absolute",
-              backgroundColor: "zinc.400",
-              zIndex: 1,
-            })}/>
-          </div>
+          <Switch />
         </div>
         <Legend
           class={css({
@@ -81,8 +26,16 @@ export function Panel(props: JSX.HTMLAttributes<HTMLDivElement>) {
           })}
         />
       </div>
-      <div>
+      <div
+        class={css({
+          display: "flex",
+          width: "100%",
+          justifyContent: "space-between",
+          alignItems: "center",
+        })}
+      >
         <h2>City Council Districts</h2>
+        <Switch />
       </div>
     </div>
   );
