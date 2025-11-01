93e15e8d7f75fc95c6cce921e1007898f9f61456
Timothy Miller <miller.tim108@gmail.com>
2025-11-01 16:33:05 -0400

    source stations from november

--- etl/minio/scripts/download.sh
+++ etl/minio/scripts/download.sh
@@ -2,5 +2,5 @@
 
 source ./scripts/spaces_alias.sh
 
-mc get spaces/tangoyankee/nyc_subway_stations/2024_aug_22_nyc_subway_stations.geojson ./data/2024_aug_22_nyc_subway_stations.geojson
+mc get spaces/tangoyankee/nyc_subway_stations/2025_nov_01_nyc_subway_stations.geojson ./data/2025_nov_01_nyc_subway_stations.geojson
 mc get spaces/tangoyankee/source_city_council_districts.geojson ./data/source_city_council_districts.geojson
