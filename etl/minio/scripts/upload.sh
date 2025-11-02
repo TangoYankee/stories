912ca337d324f28d5822a251f4fce76ac32424ee
Timothy Miller <miller.tim108@gmail.com>
2025-11-02 15:35:47 -0500

    subway line pmtiles

--- etl/minio/scripts/upload.sh
+++ etl/minio/scripts/upload.sh
@@ -3,4 +3,7 @@
 source ./scripts/spaces_alias.sh
 
 mc put data/ada-subway-stations.pmtiles spaces/tangoyankee/ada-subway-stations.pmtiles
+
+mc put data/subway-lines.pmtiles spaces/tangoyankee/subway-lines.pmtiles
+
 mc put data/city-council-districts.pmtiles spaces/tangoyankee/city-council-districts.pmtiles
